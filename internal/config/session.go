b85fc1411d39cce14fb16def01a34c692ef08148
junyonglee0223 <ljyong3339@gmail.com>
2026-05-07 18:00:34 +0900

    ✨ feat: 글로벌 세션 저장 기능 추가
    - 글로벌 세션을 JSON 형식으로 직렬화하여 저장하는 기능 구현
    - 사용자별 config 디렉토리 경로 설정 및 파일 생성 로직 추가
    - 기존 로컬 세션 저장 방식을 글로벌 세션으로 변경
    
    Ref: #S14P31A209-41

--- internal/config/session.go
+++ internal/config/session.go
@@ -7,8 +7,13 @@ import (
 	"path/filepath"
 )
 
-// Session TODO key type 고려
-type Session struct {
+const (
+	appName           = "envio"
+	globalSessionFile = "session.json"
+)
+
+// GlobalSession json으로 직렬화 해야하기 때문에 string 타입으로 설정
+type GlobalSession struct {
 	UserID     int64  `json:"userId"`
 	GithubID   string `json:"githubId"`
 	DeviceID   int64  `json:"deviceId"`
@@ -17,30 +22,55 @@ type Session struct {
 	PrivateKey string `json:"privateKey"`
 }
 
-type LocalConfig struct {
-	Session Session `json:"session"`
+type GlobalConfig struct {
+	GlobalSession GlobalSession `json:"globalSession"`
 }
 
-func SaveLocalSession(session Session) error {
-	dir := ".envio"
+func SaveGlobalSession(session GlobalSession) error {
+	path, err := GlobalSessionPath()
+	if err != nil {
+		return err
+	}
+
+	cfg := GlobalConfig{GlobalSession: session}
+
+	return writeJSONFile(path, cfg, 0600)
+}
+
+// GlobalSessionPath envio 저장 위치 설정
+//
+//	Windows: C:\Users\<사용자명>\AppData\Roaming\envio\session.json
+//	macOS:   ~/Library/Application Support/envio/session.json
+//	Linux:   ~/.config/envio/session.json
+func GlobalSessionPath() (string, error) {
+	userConfigDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", fmt.Errorf("사용자 config 디렉토리 확인 실패: %w", err)
+	}
+
+	return filepath.Join(userConfigDir, appName, globalSessionFile), nil
+}
+
+func writeJSONFile(path string, obj interface{}, perm os.FileMode) error {
+	dir := filepath.Dir(path)
 
 	if err := os.MkdirAll(dir, 0700); err != nil {
-		return fmt.Errorf(".envio 디렉토리 생성 실패: %w", err)
+		return fmt.Errorf("디렉토리 생성 실패: %w", err)
 	}
 
-	cfg := LocalConfig{
-		Session: session,
-	}
-
-	raw, err := json.MarshalIndent(cfg, "", "  ")
+	raw, err := json.MarshalIndent(obj, "", "  ")
 	if err != nil {
-		return fmt.Errorf("config 직렬화 실패: %w", err)
+		return fmt.Errorf("객체 직렬화 실패: %w", err)
 	}
 
-	path := filepath.Join(dir, "config")
+	tempPath := path + ".tmp"
 
-	if err := os.WriteFile(path, raw, 0600); err != nil {
-		return fmt.Errorf(".envio/config 저장 실패: %w", err)
+	if err := os.WriteFile(tempPath, raw, perm); err != nil {
+		return fmt.Errorf("임시 config 파일 저장 실패: %w", err)
+	}
+
+	if err := os.Rename(tempPath, path); err != nil {
+		return fmt.Errorf("config 파일 교체 실패: %w", err)
 	}
 
 	return nil
