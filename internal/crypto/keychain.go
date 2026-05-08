f98817185f173f3722ad062d9ee47473e2ddadda
junyonglee0223 <ljyong3339@gmail.com>
2026-05-08 09:20:25 +0900

    ✨ feat: 개인키 및 공개키 저장 함수 주석 보강
    - SavePrivateKey와 SavePublicKey 함수에 주석 추가
    - LoadPrivateKey 함수 주석 보강
    
    Ref: #S14P31A209-41

--- internal/crypto/keychain.go
+++ internal/crypto/keychain.go
@@ -4,17 +4,17 @@ import "github.com/zalando/go-keyring"
 
 const serviceName = "envio-cli"
 
-// 개인키 저장
+// SavePrivateKey 개인키 저장
 func SavePrivateKey(privateKeyPEM string) error {
 	return keyring.Set(serviceName, "device-private-key", privateKeyPEM)
 }
 
-// 공개키 저장
+// SavePublicKey 공개키 저장
 func SavePublicKey(publicKeyPEM string) error {
 	return keyring.Set(serviceName, "device-public-key", publicKeyPEM)
 }
 
-// 개인키 로드
+// LoadPrivateKey 개인키 로드
 func LoadPrivateKey() (string, error) {
 	return keyring.Get(serviceName, "device-private-key")
 }
